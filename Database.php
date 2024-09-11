e33a6263c02959b2e531607440f65120bbbe3418
RasinduBingusara <106803528+RasinduBingusara@users.noreply.github.com>
2024-09-11 12:52:12 +0530

    Delete Unnecessary Files

--- Database.php
+++
@@ -1,9 +0,0 @@
-<?php
-$server_name = "localhost";
-            $db_uname = "root";
-            $db_pass = "";
-            $db_name = "gallerycafe";
-         
-     //connection 
-    $conn = mysqli_connect($server_name, $db_uname, $db_pass, $db_name);
-?>
