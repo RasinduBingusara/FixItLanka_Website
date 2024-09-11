8f81920b9d7bcb217fb491ad6d7b382c705e48fd
RasinduBingusara <106803528+RasinduBingusara@users.noreply.github.com>
2024-09-11 13:10:37 +0530

    Change Database

--- Database.php
+++ Database.php
@@ -1,8 +1,8 @@
 <?php
-$server_name = "localhost";
-            $db_uname = "root";
-            $db_pass = "";
-            $db_name = "gallerycafe";
+$server_name = "testdb.cv0sgycsytfx.ap-south-1.rds.amazonaws.com";
+            $db_uname = "admin";
+            $db_pass = "0F1OD7OIlP0kMQVZyFm6";
+            $db_name = "fixitlanka";
          
      //connection 
     $conn = mysqli_connect($server_name, $db_uname, $db_pass, $db_name);
