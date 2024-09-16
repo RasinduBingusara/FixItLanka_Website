39729c05f23b86a52e83fb6a7f7f2720d3befea5
RasinduBingusara <106803528+RasinduBingusara@users.noreply.github.com>
2024-09-16 11:15:20 +0530

    Add Login and Signup Pages

---
+++ LoginValidate.php
@@ -0,0 +1,61 @@
+<?php
+session_start();
+include("Database.php");
+
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["email"]) && isset($_POST["password"])) {
+
+  $sqlVal = "SELECT UID, CID, Username, Email, Password, ContactNumber, DOB, UserType, ProfilePicture, Acc_Status FROM useraccounts WHERE Email = '" . $_POST["email"] . "'";
+
+  $result = $conn->query($sqlVal);
+
+  if ($result->num_rows > 0) {
+
+    while ($row = $result->fetch_assoc()) {
+
+      if (!password_verify($_POST["password"], $row["Password"])) {
+
+        echo '<form id="errorForm" action="Login.php" style="display:inline;">';
+        echo '<input type="hidden" name="errorMsg" value="Incorrect Username or Password">';
+        echo '</form>';
+?>
+        <script>
+          document.getElementById("errorForm").submit();
+        </script>
+
+      <?php
+
+      } else if ($row["Acc_Status"] != "Active") {
+
+        echo '<form id="errorForm" action="Login.php" style="display:inline;">';
+        echo '<input type="hidden" name="errorMsg" value="Account has been suspened">';
+        echo '</form>';
+      ?>
+        <script>
+          document.getElementById("errorForm").submit();
+        </script>
+<?php
+      } else {
+
+        $_SESSION["UserData"] = array($row["UID"],  $row["CID"], $row["Username"], $row["Email"], $row["ContactNumber"],  $row["DOB"], $row["UserType"], base64_encode($row["ProfilePicture"]));
+        echo '<script>
+        setTimeout(function() {
+          window.location.href = "Home.php";
+          }, 1000);
+          </script>';
+      }
+    }
+  }
+  else{
+    echo '<form id="errorForm" action="Login.php" style="display:inline;">';
+        echo '<input type="hidden" name="errorMsg" value="Incorrect Username or Password">';
+        echo '</form>';
+?>
+        <script>
+          document.getElementById("errorForm").submit();
+        </script>
+
+      <?php
+  }
+  
+}
+?>
--- SignUpValidate.php
+++ SignUpValidate.php
@@ -1,32 +1,46 @@
 <?php
 include("Database.php");
 
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["email"]) && isset($_POST["password"]) && isset($_POST["username"]) && isset($_POST["phone"]) && isset($_POST["profilepicture"])) {
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["email"]) && isset($_POST["password"]) && isset($_POST["firstName"]) && isset($_POST["lastName"]) && isset($_POST["contactNumber"])) {
 
   $sqlVal = "SELECT UID FROM useraccounts WHERE Email = '" . $_POST["email"] . "'";
-
   $result = $conn->query($sqlVal);
 
   if ($result->num_rows > 0) {
-    echo "<script>alert('Email Address Already Exist');</script>";
+    echo '<form id="errorForm" action="Signup.php" style="display:inline;">';
+    echo '<input type="hidden" name="errorMsg" value="Email Address Already Exist">';
+    echo '</form>';
+?>
+    <script>
+      document.getElementById("errorForm").submit();
+    </script>
+    <?php
   } else {
 
     $password = password_hash($_POST["password"], PASSWORD_DEFAULT);
 
     $currentTimestamp = time();
     $currentDateTime = date('Y-m-d H:i:s', $currentTimestamp);
+    $username = $_POST["firstName"] . $_POST["lastName"];
 
-    $sql = "INSERT INTO useraccounts(CID, Username, Password, Email, ContactNumber, DOB, Area, ProfilePicture, UserType, Acc_Status, Created_At) VALUES ('2','" . $_POST["username"] . "','" . $password . "','" . $_POST["email"] . "','" . $_POST["phone"] . "','2024-09-11','Colombo','','User','Active','" . $currentDateTime . "')";
+    $sql = "INSERT INTO useraccounts(CID, Username, Password, Email, ContactNumber, DOB, Area, ProfilePicture, UserType, Acc_Status, Created_At) VALUES ('2','" . $username . "','" . $password . "','" . $_POST["email"] . "','" . $_POST["contactNumber"] . "','2024-09-11','Colombo','','User','Active','" . $currentDateTime . "')";
 
     if (mysqli_query($conn, $sql)) {
 
       echo "<script>
         setTimeout(function() {
-          window.location.href = 'SignUp.php';
+          window.location.href = 'login.php';
         }, 1000);
       </script>";
     } else {
-      echo "<script>alert('Error Creating Account');</script>";
+      echo '<form id="errorForm" action="Signup.php" style="display:inline;">';
+    echo '<input type="hidden" name="errorMsg" value=""Error Creating Account">';
+    echo '</form>';
+    ?>
+      <script>
+        document.getElementById("errorForm").submit();
+      </script>
+<?php
     }
   }
 }
