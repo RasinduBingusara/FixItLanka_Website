34cc2bbae38e779607501525ad0354ccb81e9702
RasinduBingusara <106803528+RasinduBingusara@users.noreply.github.com>
2024-10-07 18:02:31 +0530

    Supervisor panel css fixed

--- SupervisorPanel.php
+++ SupervisorPanel.php
@@ -78,7 +78,7 @@ $conn->close();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Supervisor Dashboard</title>
-    <link rel="stylesheet" href="css/SupervisorDashboard.css">
+    <link rel="stylesheet" href="css/supervisorDashboard.css">
 </head>
 <body>
 
