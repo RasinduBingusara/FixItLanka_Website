ca165d0e511fa8820bc3547f39157e9be2fbd271
RasinduBingusara <106803528+RasinduBingusara@users.noreply.github.com>
2024-10-14 10:53:09 +0530

    Database Change

--- Main/Database.php
+++ Main/Database.php
@@ -1,15 +1,15 @@
 <?php
-$server_name = "localhost";
-$db_uname = "root";
-$db_pass = "";
-$db_name = "fixitlanka";
+// $server_name = "localhost";
+// $db_uname = "root";
+// $db_pass = "";
+// $db_name = "fixitlanka";
 
-$conn = mysqli_connect($server_name, $db_uname, $db_pass, $db_name);
-
-// $server_name = "testdb.cv0sgycsytfx.ap-south-1.rds.amazonaws.com";
-//         $db_uname = "admin";
-//         $db_pass = "0F1OD7OIlP0kMQVZyFm6";
-//         $db_name = "fixitlanka";
-
-//  //connection 
 // $conn = mysqli_connect($server_name, $db_uname, $db_pass, $db_name);
+
+$server_name = "testdb.cv0sgycsytfx.ap-south-1.rds.amazonaws.com";
+        $db_uname = "admin";
+        $db_pass = "0F1OD7OIlP0kMQVZyFm6";
+        $db_name = "fixitlanka";
+
+ //connection 
+$conn = mysqli_connect($server_name, $db_uname, $db_pass, $db_name);
